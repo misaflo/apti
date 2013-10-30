20f43276122e412498ea3acb143c6d9f63cee1c4
Florent Lévigne <florent.levigne@obspm.fr>
2013-10-30 14:20:49 +0100

    add comments, Color.end

--- src/config/Color.rb
+++ src/config/Color.rb
@@ -2,8 +2,23 @@
 # encoding: utf-8
 
 module Config
+
+  # Shell colors code.
   class Color
+    # @!attribute install
+    #   @return [String] Color of install.
+    #
+    # @!attribute remove
+    #   @return [String] Color of remove.
+    #
+    # @!attribute description
+    #   @return [String] Color of description.
+    #
+    # @!attribute end [r]
+    #   @return [String] Constant for stopping color.
+
     attr_accessor :install, :remove, :description
+
+    attr_reader :end
   end
 end
-
