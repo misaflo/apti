39d8364c6deb3963375b2249b84b7f444d8fa9f4
Florent Lévigne <florent.levigne@obspm.fr>
2013-10-31 14:55:16 +0100

    add (c), AUTHORS file

--- src/apti/Apti.rb
+++ src/apti/Apti.rb
@@ -1,5 +1,25 @@
-#!/usr/bin/env ruby
-# encoding: utf-8
+#===============================================================================
+#
+# This file is part of Apti.
+#
+# Copyright (C) 2012-2013 by Florent Lévigne <florent.levigne at mailoo dot com>
+# Copyright (C) 2013 by Julien Rosset <jul.rosset at gmail dot com>
+#
+#
+# Apti is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Apti is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#===============================================================================
 
 module Apti
 
@@ -166,5 +186,3 @@ module Apti
     end
   end
 end
-
-
